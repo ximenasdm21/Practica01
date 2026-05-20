5bdaa90b4bd26fa75330f9e6b0f2fd2c6a9d1b9a
ximenasdm21 <xdominguezm@est.ups.edu.ec>
2026-05-20 10:11:25 -0500

    Add files via upload

---
+++ Persona.java
@@ -0,0 +1,24 @@
+public class Persona {
+    private String nombre;
+    private int edad;
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setEdad(int edad) {
+        if (edad >= 0) {
+            this.edad = edad;
+        } else {
+            System.out.println("La edad no puede ser negativa.");
+        }
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+}
